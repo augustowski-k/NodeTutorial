ae19554f459413ee7fb9db7798c71d74efae93c2
krzysztof <krzysztof.augustowski@pollub.edu.pl>
2018-02-24 14:04:12 +0100

    Blog app completed

--- blogApp/app.js
+++ blogApp/app.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var sanitizer = require('express-sanitizer');
 
 var app = express();
 mongoose.connect('mongodb://localhost/blog_app');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(sanitizer());
+app.use(methodOverride('_method'));
 
 var blogSchema = new mongoose.Schema({
   title: { type: String, required: true },
@@ -37,6 +41,7 @@ app.get('/blogs/new', function (req, res) {
 
 app.post('/blogs', function (req, res) {
   var blogData = req.body.blog;
+  blogData.body = req.sanitize(blogData.body);
   Blog.create(blogData, function (err, blog) {
     if (err) {
       console.log(err);
@@ -64,6 +69,24 @@ app.get('/blogs/:id/edit', function (req, res) {
     } else {
       res.render('edit', postData);
     }
+  });
+});
+
+app.put('/blogs/:id', function (req, res) {
+  var blogData = req.body.blog;
+  blogData.body = req.sanitize(blogData.body);
+  Blog.findByIdAndUpdate(req.params.id, blogData, function (err, blogData) {
+    if (err) {
+      res.send('Error');
+    } else {
+      res.redirect('/blogs/' + blogData._id);
+    }
+  });
+});
+
+app.delete('/blogs/:id', function (req, res) {
+  Blog.findByIdAndRemove(req.params.id, function (err) {
+    res.redirect('/blogs');
   });
 });
 
