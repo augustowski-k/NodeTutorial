c18e1df7509d12cb96e432cfa29f7e6ab0469ea3
krzysztof <krzysztof.augustowski@pollub.edu.pl>
2018-03-24 08:30:27 +0100

    yelpClone - comment edit, delete and authorization

--- yelpCamp/routes/comments.js
+++ yelpCamp/routes/comments.js
@@ -42,11 +42,53 @@ router.post('/', isLoggedIn, function(req, res) {
   });
 });
 
+router.get('/:comId/edit', checkCommentOwnership, function(req, res){
+  res.render('comments/edit', {comment: req.comment, campId: req.params.id});
+});
+
+router.put('/:comId', checkCommentOwnership, function(req, res) {
+  console.log(req.body.comment);
+  Comment.findByIdAndUpdate(req.params.comId, req.body.comment, function(err, comment){
+    if(err){
+      res.redirect('back');
+    }else{
+      res.redirect('/campgrounds/' + req.params.id);
+    }
+  });
+});
+
+router.delete('/:comId', checkCommentOwnership, function(req, res) {
+  req.comment.remove(function(err, com){
+    if(err){
+      res.redirect('back');
+    }else{
+      res.redirect('/campgrounds/' + req.params.id);
+    }
+  });
+});
+
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
   res.redirect('/login/?url=' + req.originalUrl);
+}
+
+function checkCommentOwnership(req, res, next){
+  if(req.isAuthenticated()){
+    Comment.findById(req.params.comId, function(err, comment){
+      if(err){
+        res.redirect('back');
+      }else if(comment.author.id.equals(req.user._id)){
+        req.comment = comment;
+        next();
+      }else{
+        res.redirect('back');
+      }
+    });
+  }else {
+    res.redirect('back');
+  }
 }
 
 module.exports = router;
